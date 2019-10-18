e6e2ecdde7de277a87d60b74eaf4cde868398656
Joby-Sunny <jobys39@live.com>
2019-10-18 11:11:56 +0530

    Migration Of Todo Table done

--- models/Todo.js
+++ models/Todo.js
@@ -1,19 +1,19 @@
-const Sequeize = require('sequelize');
+const sequelize = require('sequelize');
 const {sequelize} = require('../libraries');
 
 module.exports = sequelize.define('Todo', {
   id: {
-    type: Sequeize.INTEGER(11),
+    type: sequelize.INTEGER(11),
     allowNull: false,
     autoIncrement: true,
     primaryKey: true
   },
   title: {
-    type: Sequeize.STRING(120),
+    type: sequelize.STRING(120),
     allowNull: false
   },
   description: {
-    type: Sequeize.STRING(420),
+    type: sequelize.STRING(420),
     allowNull: false
   },
 });
